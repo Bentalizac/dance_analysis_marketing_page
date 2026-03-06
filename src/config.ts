7b7aeb26af06c825e39d485a14688b072a6549dc
Eli <bentalizac@gmail.com>
2026-03-05 21:38:46 -0700

    New name ideas

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
-export const PRODUCT_NAME = "RoutineLeigh";
+export const PRODUCT_NAME = "StepChange";
+export const ALT_NAME =
+  "Floorcraft, DanceDoc, SambaSurgeon, WaltzWellness, ChaChaCheckup, FoxtrotFixer, TangoTechnician";
+export const ALT_ALT_NAME = "BeBetter, SkillIssue, FoxtrotFuego, OneMoreTime";
